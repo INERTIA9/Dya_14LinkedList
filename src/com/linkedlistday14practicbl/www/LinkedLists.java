7423ea0919c4374411ffc1046b320727aa6ab486
unknown <dekapurwars@gmail.com>
2021-09-22 14:08:24 +0530

    Welcome Statement

---
+++ src/com/linkedlistday14practicbl/www/LinkedLists.java
@@ -0,0 +1,12 @@
+package com.linkedlistday14practicbl.www;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class LinkedLists {
+	public static void main(String[] arg) {
+		System.out.println("Welcome to Linkedlist Problem Statement");
+	}
+}
